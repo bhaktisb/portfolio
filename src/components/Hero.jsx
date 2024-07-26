609dd31039ac5c3640b9b90b0a93be17012aff91
Moink25 <khanmoin9570@gmail.com>
2024-07-26 14:19:36 +0530

    added social links

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,13 @@
-import React, { useEffect, useRef } from 'react';
-import { motion } from 'framer-motion';
+import React, { useEffect, useRef } from "react";
+import { motion } from "framer-motion";
 import { SiLeetcode } from "react-icons/si";
 import { FaSquareGithub } from "react-icons/fa6";
 import { FaTwitter, FaInstagramSquare } from "react-icons/fa";
-import profileImage from '../assets/img/m.png'; // Replace with your actual image path
+import profileImage from "../assets/img/m.png"; // Replace with your actual image path
 
 const Hero = () => {
-  const svgBase64 = 'PCEtLT94bWwgdmVyc2lvbj0iMS4wIiBzdGFuZGFsb25lPSJubyI/LS0+CiAgICAgICAgICAgICAgPHN2ZyBpZD0ic3ctanMtYmxvYi1zdmciIHZpZXdCb3g9IjAgMCAxMDAgMTAwIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZlcnNpb249IjEuMSI+CiAgICAgICAgICAgICAgICAgICAgPGRlZnM+IAogICAgICAgICAgICAgICAgICAgICAgICA8bGluZWFyR3JhZGllbnQgaWQ9InN3LWdyYWRpZW50IiB4MT0iMCIgeDI9IjEiIHkxPSIxIiB5Mj0iMCI+CiAgICAgICAgICAgICAgICAgICAgICAgICAgICA8c3RvcCBpZD0ic3RvcDEiIHN0b3AtY29sb3I9InJnYmEoMjQ4LCAxODQuNTc1LCA1NSwgMSkiIG9mZnNldD0iMCUiPjwvc3RvcD4KICAgICAgICAgICAgICAgICAgICAgICAgICAgIDxzdG9wIGlkPSJzdG9wMiIgc3RvcC1jb2xvcj0icmdiYSgyNTUsIDIxMSwgMCwgMC43KSIgb2Zmc2V0PSIxMDAlIj48L3N0b3A+CiAgICAgICAgICAgICAgICAgICAgICAgIDwvbGluZWFyR3JhZGllbnQ+CiAgICAgICAgICAgICAgICAgICAgPC9kZWZzPgogICAgICAgICAgICAgICAgPHBhdGggZmlsbD0idXJsKCNzdy1ncmFkaWVudCkiIGQ9Ik0yMy43LC0zNi44QzMwLC0zMi45LDMzLjksLTI1LjEsMzYuNywtMTcuM0MzOS41LC05LjUsNDEuMSwtMS42LDM4LDRDMzQuOSw5LjYsMjcsMTMuMSwyMS4yLDE2LjZDMTUuNCwyMC4xLDExLjcsMjMuNyw3LjEsMjUuNkMyLjUsMjcuNiwtMi45LDI4LC03LjcsMjYuNEMtMTIuNCwyNC45LC0xNi41LDIxLjQsLTIyLjIsMTcuN0MtMjcuOCwxMy45LC0zNSwxMCwtMzgsNC4xQy00MSwtMS44LC0zOS44LC05LjcsLTM2LjcsLTE2LjlDLTMzLjYsLTI0LjEsLTI4LjcsLTMwLjUsLTIyLjIsLTM0LjRDLTE1LjgsLTM4LjMsLTcuOSwtMzkuNywwLjQsLTQwLjNDOC43LC00MSwxNy40LC00MC44LDIzLjcsLTM2LjhaIiB3aWR0aD0iMTAwJSIgaGVpZ2h0PSIxMDAlIiB0cmFuc2Zvcm09InRyYW5zbGF0ZSg1MCA1MCkiIHN0eWxlPSJ0cmFuc2l0aW9uOiAwLjNzOyIgc3Ryb2tlLXdpZHRoPSIwIj48L3BhdGg+CiAgICAgICAgICAgICAgPC9zdmc+';
+  const svgBase64 =
+    "PCEtLT94bWwgdmVyc2lvbj0iMS4wIiBzdGFuZGFsb25lPSJubyI/LS0+CiAgICAgICAgICAgICAgPHN2ZyBpZD0ic3ctanMtYmxvYi1zdmciIHZpZXdCb3g9IjAgMCAxMDAgMTAwIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZlcnNpb249IjEuMSI+CiAgICAgICAgICAgICAgICAgICAgPGRlZnM+IAogICAgICAgICAgICAgICAgICAgICAgICA8bGluZWFyR3JhZGllbnQgaWQ9InN3LWdyYWRpZW50IiB4MT0iMCIgeDI9IjEiIHkxPSIxIiB5Mj0iMCI+CiAgICAgICAgICAgICAgICAgICAgICAgICAgICA8c3RvcCBpZD0ic3RvcDEiIHN0b3AtY29sb3I9InJnYmEoMjQ4LCAxODQuNTc1LCA1NSwgMSkiIG9mZnNldD0iMCUiPjwvc3RvcD4KICAgICAgICAgICAgICAgICAgICAgICAgICAgIDxzdG9wIGlkPSJzdG9wMiIgc3RvcC1jb2xvcj0icmdiYSgyNTUsIDIxMSwgMCwgMC43KSIgb2Zmc2V0PSIxMDAlIj48L3N0b3A+CiAgICAgICAgICAgICAgICAgICAgICAgIDwvbGluZWFyR3JhZGllbnQ+CiAgICAgICAgICAgICAgICAgICAgPC9kZWZzPgogICAgICAgICAgICAgICAgPHBhdGggZmlsbD0idXJsKCNzdy1ncmFkaWVudCkiIGQ9Ik0yMy43LC0zNi44QzMwLC0zMi45LDMzLjksLTI1LjEsMzYuNywtMTcuM0MzOS41LC05LjUsNDEuMSwtMS42LDM4LDRDMzQuOSw5LjYsMjcsMTMuMSwyMS4yLDE2LjZDMTUuNCwyMC4xLDExLjcsMjMuNyw3LjEsMjUuNkMyLjUsMjcuNiwtMi45LDI4LC03LjcsMjYuNEMtMTIuNCwyNC45LC0xNi41LDIxLjQsLTIyLjIsMTcuN0MtMjcuOCwxMy45LC0zNSwxMCwtMzgsNC4xQy00MSwtMS44LC0zOS44LC05LjcsLTM2LjcsLTE2LjlDLTMzLjYsLTI0LjEsLTI4LjcsLTMwLjUsLTIyLjIsLTM0LjRDLTE1LjgsLTM4LjMsLTcuOSwtMzkuNywwLjQsLTQwLjNDOC43LC00MSwxNy40LC00MC44LDIzLjcsLTM2LjhaIiB3aWR0aD0iMTAwJSIgaGVpZ2h0PSIxMDAlIiB0cmFuc2Zvcm09InRyYW5zbGF0ZSg1MCA1MCkiIHN0eWxlPSJ0cmFuc2l0aW9uOiAwLjNzOyIgc3Ryb2tlLXdpZHRoPSIwIj48L3BhdGg+CiAgICAgICAgICAgICAgPC9zdmc+";
 
   const svgRef = useRef(null);
 
@@ -17,7 +18,10 @@ const Hero = () => {
   }, [svgBase64]);
 
   return (
-    <div className="w-full flex flex-col md:flex-row items-center md:justify-between" id='hero'>
+    <div
+      className="w-full flex flex-col md:flex-row items-center md:justify-between"
+      id="hero"
+    >
       {/* Left part */}
       <div className="md:w-[50%] flex justify-center items-center">
         <motion.div
@@ -51,7 +55,7 @@ const Hero = () => {
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8 }}
         >
-          Hi, Myself <span className='text-[#ffdf00] font-bold'>Moin Khan</span>
+          Hi, Myself <span className="text-[#ffdf00] font-bold">Moin Khan</span>
         </motion.h1>
         <div className="flex justify-start">
           <motion.p
@@ -69,28 +73,48 @@ const Hero = () => {
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1.2 }}
         >
-          Passionate MERN stack developer and machine learning enthusiast, creating innovative solutions that blend technology and user experience.
+          Passionate MERN stack developer and machine learning enthusiast,
+          creating innovative solutions that blend technology and user
+          experience.
         </motion.p>
-        <button className="bg-[#ffdf00] w-[200px] rounded-md font-medium my-6 mx-auto px-4 py-3 text-black">Go to MyTechStack</button>
+        <button className="bg-[#ffdf00] w-[200px] rounded-md font-medium my-6 mx-auto px-4 py-3 text-black">
+          Go to MyTechStack
+        </button>
 
         {/* Social Icons */}
         <div className="flex space-x-4 mt-4 justify-start">
-          <a href="https://leetcode.com" target="_blank" rel="noopener noreferrer">
-            <SiLeetcode className='text-black text-3xl shadow-md shadow-[#ffdf00]' />
+          <a
+            href="https://leetcode.com/u/moink25/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <SiLeetcode className="text-black text-3xl shadow-md shadow-[#ffdf00]" />
           </a>
-          <a href="https://github.com" target="_blank" rel="noopener noreferrer">
-            <FaSquareGithub className='text-black text-3xl shadow-md shadow-[#ffdf00]' />
+          <a
+            href="https://github.com/Moink25/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaSquareGithub className="text-black text-3xl shadow-md shadow-[#ffdf00]" />
           </a>
-          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-            <FaTwitter className='text-black text-3xl shadow-md shadow-[#ffdf00]' />
+          <a
+            href="https://twitter.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaTwitter className="text-black text-3xl shadow-md shadow-[#ffdf00]" />
           </a>
-          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-            <FaInstagramSquare className='text-black text-3xl shadow-md shadow-[#ffdf00]' />
+          <a
+            href="https://www.instagram.com/moink_25?igsh=MTV2bzUzY2U5aWZjMg=="
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaInstagramSquare className="text-black text-3xl shadow-md shadow-[#ffdf00]" />
           </a>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Hero;
